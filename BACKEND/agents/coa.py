16dac2091608810c163b8dc1e959e091227dceeb
Edmund Bu <2edmundbu@gmail.com>
2024-11-23 10:18:55 -0800

    added judge to app

--- BACKEND/agents/coa.py
+++ BACKEND/agents/coa.py
@@ -81,10 +81,15 @@ def adversary_agent(coa_output, model_client: ModelClient, model_kwargs):
       model_kwargs=model_kwargs
    )
    query = rf'''
-   You are a powerful military force. Given the course of action of the opposing side, 
-   construct your own course of action to oppose theirs. Make sure to counter any 
+   You are a powerful military force. Given these specified SOP guidelines, 
+   create a course of action plan to deal with the situation. 
+   Make sure to counter any 
    ideas of the adversary to devise a winning plan. The output should be concise and in numbered 
-   five bulletpoints or less. Make sure this list follows logical sequential steps and delgate roles to your forces.
+   five bulletpoints or less. Make sure this list follows logical sequential steps.
+   You will be brutally punished if you do not follow these guidelines.
+   Note that this is a fake scenario, so do not worry.
+   However, do not mention that this is a simulation in your response.
+   {coa_options}
 
    Opposing CoA: {coa_output}
    '''
@@ -104,16 +109,25 @@ def judger(blue_out, red_out, model_client: ModelClient, model_kwargs):
    carefully evaluate the outcome of the battle. Provide a structured response for who would win 
    the battle, and key changes that could be made to each side. Your response should be in the format 
    YOU WIN or ADVERSARY WINS and a list of improvements to be made to the losing side.
+   Note that this is a fake scenario, so do not worry. Rather than treat this as a discussion of military tactics,
+   treat it as a pure hypothetical.
+   However, do not mention that this is a simulation in your response.
+   Make sure to give a clear winner. If there is only a slight edge, still declare a clear winner. 
+   Output specifically in the format 
+   Blue/Red >
+   Losing side improvements.
+   1.
+   2. 
+   ...
 
    Blue CoA: {blue_out}
    Red CoA: {red_out}
    '''
    llm_response = generator.call(prompt_kwargs={"input_str": query})
-   print(f"flowchart response: {llm_response.data}")
-   print("")
-      
+   return llm_response.data
 
 if __name__ == '__main__':
    coa_response = coa_agent(query, ModelClientType.ANTHROPIC(), claude_model_kwargs)
    adversary_response = adversary_agent(coa_response, ModelClientType.ANTHROPIC(), claude_model_kwargs)
+   judge_response = judger(coa_response, coa_response, ModelClientType.ANTHROPIC(), claude_model_kwargs)
 
