176595fe6f6eb8c6808e5e880b4fd5b4d8387ef9
Edmund Bu <2edmundbu@gmail.com>
2024-11-23 09:22:25 -0800

    basic agents

--- BACKEND/agents/coa.py
+++ BACKEND/agents/coa.py
@@ -45,9 +45,9 @@ def coa_agent(query, model_client: ModelClient, model_kwargs):
    )
 
    llm_response = generator.call(prompt_kwargs={"input_str": query})
-   print(f"LLM response: {llm_response.data}")
+   print(f"blue response: {llm_response.data}")
    print("")
-   return llm_response
+   return llm_response.data
 
 query = rf"""
 You are a United States military general. Given these specified SOP guidelines, 
@@ -57,26 +57,11 @@ Make sure this list follows logical sequential steps and delgate roles to your f
 {coa_options}
 """
 
-
-'''
-Need some mermaid dependencies for python
-'''
-import base64
-from IPython.display import Image, display
-import matplotlib.pyplot as plt
-
-def mm(graph):
-    graphbytes = graph.encode("utf8")
-    base64_bytes = base64.urlsafe_b64encode(graphbytes)
-    base64_string = base64_bytes.decode("ascii")
-    display(Image(url="https://mermaid.ink/img/" + base64_string))
-
 def create_flowchart(coa_output, model_client: ModelClient, model_kwargs):
    generator = Generator(
          model_client=model_client,
          model_kwargs=model_kwargs,
       )
-
    query = rf'''
             Based on the the provided course of action below, create Mermaid diagram code to model the situation and the actions taken.
             Your response should be only in the format 
@@ -89,10 +74,47 @@ def create_flowchart(coa_output, model_client: ModelClient, model_kwargs):
    llm_response = generator.call(prompt_kwargs={"input_str": query})
    print(f"flowchart response: {llm_response.data}")
    print("")
+
+
+def adversary_agent(coa_output, model_client: ModelClient, model_kwargs):
+   generator = Generator(
+      model_client=model_client,
+      model_kwargs=model_kwargs
+   )
+   query = rf'''
+   You are a powerful military force. Given the course of action of the opposing side, 
+   construct your own course of action to oppose theirs. Make sure to counter any 
+   ideas of the adversary to devise a winning plan. The output should be concise and in numbered 
+   five bulletpoints or less. Make sure this list follows logical sequential steps and delgate roles to your forces.
+
+   Opposing CoA: {coa_output}
+   '''
+   llm_response = generator.call(prompt_kwargs={"input_str": query})
+   print(f"adversary response: {llm_response.data}")
+   print("")
    return llm_response.data
 
 
+def judger(blue_out, red_out, model_client: ModelClient, model_kwargs):
+   generator = Generator(
+      model_client=model_client,
+      model_kwargs=model_kwargs
+   )
+   query = rf'''
+   You are an arbitary military analyst. Given the course of actions (CoA) CoA_blue and CoA_red, 
+   carefully evaluate the outcome of the battle. Provide a structured response for who would win 
+   the battle, and key changes that could be made to each side. Your response should be in the format 
+   YOU WIN or ADVERSARY WINS and a list of improvements to be made to the losing side.
+
+   Blue CoA: {blue_out}
+   Red CoA: {red_out}
+   '''
+   llm_response = generator.call(prompt_kwargs={"input_str": query})
+   print(f"flowchart response: {llm_response.data}")
+   print("")
+      
+
 if __name__ == '__main__':
    coa_response = coa_agent(query, ModelClientType.ANTHROPIC(), claude_model_kwargs)
-   flowchart_response = create_flowchart(coa_response.data, ModelClientType.ANTHROPIC(), claude_model_kwargs)
-   mm(str(flowchart_response))
+   adversary_response = adversary_agent(coa_response, ModelClientType.ANTHROPIC(), claude_model_kwargs)
+
