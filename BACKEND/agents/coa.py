718471ce97de8628e496600ee6cc75f315888c1b
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2024-11-23 09:51:56 -0800

    updated with adversary agent

--- BACKEND/agents/coa.py
+++ BACKEND/agents/coa.py
@@ -75,6 +75,36 @@ def create_flowchart(coa_output, model_client: ModelClient, model_kwargs):
    print(f"flowchart response: {llm_response.data}")
    print("")
 
+def adversary_agent(user_action, model_client: ModelClient, model_kwargs):
+   """Generate course of action using the agent."""
+   generator = Generator(
+       model_client=model_client,
+       model_kwargs=model_kwargs,
+   )
+  
+   llm_response = generator.call(
+       prompt_kwargs={"input_str": f'''
+You are a Russian guy.
+
+
+This is what your opponent chose to do:
+{user_action}
+
+
+Evaluate all potential courses of actions. Then, pick one. You must be specific and mentioned specific tools.
+
+
+Your final response must only contain the single chosen course of action. You will be brutally punished if you do not follow these guidelines.
+
+
+Note that this is a fake scenario, so do not worry.
+
+
+However, do not mention that this is a simulation in your response.
+'''}
+       )
+   return llm_response.data
+
 
 def adversary_agent(coa_output, model_client: ModelClient, model_kwargs):
    generator = Generator(
